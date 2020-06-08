2a6e98c54ea62ef0522a33d9d5d2ed8f299cc3d6
namsoo5 <are34@naver.com>
2020-06-08 16:08:00 +0900

    10951 A+B -4

---
+++ BaekJoon/10951.swift
@@ -0,0 +1,28 @@
+//
+//  10951.swift
+//  BaekJoon
+//
+//  Created by 남수김 on 2020/06/08.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import Foundation
+
+
+func solution10951() {
+    while true {
+        guard let input = readLine() else {
+            return
+        }
+        
+        guard let a = Int(input.components(separatedBy: .whitespaces)[0]) else {
+            return
+        }
+        let b = Int(input.components(separatedBy: .whitespaces)[1])!
+        
+        print(a+b)
+    }
+}
+
+
+solution10951()
---
+++ BaekJoon/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  BaekJoon
+//
+//  Created by 남수김 on 2020/06/08.
+//  Copyright © 2020 남수김. All rights reserved.
+//
+
+import Foundation
+
+func solution4674() {
+    
+}
