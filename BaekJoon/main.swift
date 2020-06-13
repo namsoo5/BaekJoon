fb63d2a5d851be1c99c3ebf5388aabae8fc1b124
namsoo5 <are34@naver.com>
2020-06-13 14:51:31 +0900

    UPDATE: 2292 벌집

--- BaekJoon/main.swift
+++ BaekJoon/main.swift
@@ -11,8 +11,21 @@ import Foundation
 let startTime = CFAbsoluteTimeGetCurrent()
 
 func solution2292() {
+    var origin = 1
+    let distance = 6
     
+    let input = Int(readLine()!)!
+    
+    for i in 0... {
+        origin += distance * i
+        if origin >= input {
+            print("\(i+1)")
+            return
+        }
+    }
 }
+
+solution2292()
 
 let endTime = CFAbsoluteTimeGetCurrent() - startTime
 print("\(endTime)s")
