850b9aa6c8a557915bc15089412e7674ccd38ea5
김남수 <are34@naver.com>
2021-05-26 18:41:14 +0900

    UPDATE: LeetCode 20210524

---
+++ BaekJoon/2021년/5월/LeetCode20210524.swift
@@ -0,0 +1,14 @@
+//
+//  LeetCode20210524.swift
+//  BaekJoon
+//
+//  Created by 남수김 on 2021/05/25.
+//  Copyright © 2021 남수김. All rights reserved.
+//
+
+import Foundation
+
+private func toLowerCase(_ s: String) -> String {
+    return s.lowercased()
+}
+
